35e11f2b6abe1266c013caad646828dffa2583f1
Thomas Crews <twcrews96@gmail.com>
2020-08-16 23:17:03 -0500

    Added FormatTimespan and Capitalize methods.

--- UX/Formatting.cs
+++ UX/Formatting.cs
@@ -14,6 +14,18 @@ namespace Crews.UX
             {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "nineteen"}
         };
 
+        private static readonly Dictionary<string, int> PrecisionMap = new Dictionary<string, int>()
+        {
+            {TimeUnit.Year, 0}, {TimeUnit.Month, 1}, {TimeUnit.Week, 2}, {TimeUnit.Day, 3 }, {TimeUnit.Hour, 4}, 
+            {TimeUnit.Minute, 5}, {TimeUnit.Second, 6}
+        };
+
+        /// <summary>
+        /// Formats an integer between 1 and 99 (inclusive) as text. 
+        /// Other integers return a ToString of themselves.
+        /// </summary>
+        /// <param name="number">The integer to format.</param>
+        /// <returns>Returns a string representing the number.</returns>
         public static string NumberAsText(int number)
         {
             if (number < 1 || number > 99)
@@ -40,14 +52,16 @@ namespace Crews.UX
             return number.ToString();
         }
 
+        #region "FormatQuantity"
+
         /// <summary>
-        /// Formats a quantity with a given unit.
+        /// Formats a double with a given unit.
         /// </summary>
         /// <param name="quantity">The value of the quantity.</param>
         /// <param name="unit">The unit of measurement.</param>
         /// <param name="textQuantity">Indicates whether the quantity number should be converted to text.</param>
         /// <returns>Returns a string representing the formatted quantity and unit of measurement.</returns>
-        public static string QuantityFormat(double quantity, string unit, bool textQuantity)
+        public static string FormatQuantity(double quantity, string unit, bool textQuantity)
         {
             string prefix = string.Empty;
             string quantityStr;
@@ -67,7 +81,7 @@ namespace Crews.UX
                         prefix = "about";
                     }
                 }
-                quantityStr = NumberAsText((int)quantity);
+                quantityStr = NumberAsText((int)Math.Ceiling(quantity));
             }
             else
             {
@@ -94,22 +108,213 @@ namespace Crews.UX
             return string.Format("{0} {1} {2}", prefix, quantityStr, unitStr).Trim();
         }
 
-
-        public static string QuantityFormat(int quantity, string unit, bool textQuantity)
+        /// <summary>
+        /// Formats an integer with a given unit.
+        /// </summary>
+        /// <param name="quantity">The value of the quantity.</param>
+        /// <param name="unit">The unit of measurement.</param>
+        /// <param name="textQuantity">Indicates whether the quantity number should be converted to text.</param>
+        /// <returns>Returns a string representing the formatted quantity and unit of measurement.</returns>
+        public static string FormatQuantity(int quantity, string unit, bool textQuantity)
         {
-            return QuantityFormat((double)quantity, unit, textQuantity);
+            return FormatQuantity((double)quantity, unit, textQuantity);
         }
 
-
-        public static string QuantityFormat(double quantity, string unit)
+        /// <summary>
+        /// Formats a double with a given unit.
+        /// </summary>
+        /// <param name="quantity">The value of the quantity.</param>
+        /// <param name="unit">The unit of measurement.</param>
+        /// <param name="textQuantity">Indicates whether the quantity number should be converted to text.</param>
+        /// <returns>Returns a string representing the formatted quantity and unit of measurement.</returns>
+        public static string FormatQuantity(double quantity, string unit)
         {
-            return QuantityFormat(quantity, unit, false);
+            return FormatQuantity(quantity, unit, false);
         }
 
-
-        public static string QuantityFormat(int quantity, string unit)
+        /// <summary>
+        /// Formats an integer with a given unit.
+        /// </summary>
+        /// <param name="quantity">The value of the quantity.</param>
+        /// <param name="unit">The unit of measurement.</param>
+        /// <returns>Returns a string representing the formatted quantity and unit of measurement.</returns>
+        public static string FormatQuantity(int quantity, string unit)
         {
-            return QuantityFormat((double)quantity, unit, false);
+            return FormatQuantity((double)quantity, unit, false);
         }
+
+        #endregion
+
+        #region "FormatTimeSpan"
+
+        /// <summary>
+        /// Formats a TimeSpan as text.
+        /// </summary>
+        /// <param name="timeSpan">The TimeSpan object to format.</param>
+        /// <param name="precision">The TimeUnit of precision to use.</param>
+        /// <param name="textQuantity">Indicates whether the TimeSpan values should be converted to text.</param>
+        /// <returns>Returns a string representation of the TimeSpan.</returns>
+        public static string FormatTimespan(TimeSpan timeSpan, TimeUnit precision, bool textQuantity)
+        {
+            if (timeSpan.TotalSeconds <= 0)
+            {
+                throw new ArgumentException("Cannot format zero or negative TimeStamps.");
+            }
+
+            string returnString = string.Empty;
+
+            int years = 0;
+            int months = 0;
+            int weeks = 0;
+
+            if (timeSpan.TotalDays >= 365)
+            {
+                years = timeSpan.Days / 365;
+                returnString += FormatQuantity(years, TimeUnit.Year, textQuantity) + ", ";
+            }
+            else if (PrecisionMap[precision] == 0)
+            {
+                return FormatQuantity(timeSpan.TotalDays / 365, TimeUnit.Year, textQuantity);
+            }
+
+            if (timeSpan.TotalDays >= 30 && PrecisionMap[precision] >= 1)
+            {
+                months = timeSpan.Days / 30 - years * 12;
+                returnString += months > 0 ? 
+                    FormatQuantity(months, TimeUnit.Month, textQuantity) + ", " : string.Empty;
+            }
+            else if (PrecisionMap[precision] == 1)
+            {
+                return FormatQuantity(timeSpan.TotalDays / 30, TimeUnit.Month, textQuantity);
+            }
+
+            if (timeSpan.TotalDays >= 7 && PrecisionMap[precision] >= 2)
+            {
+                weeks = timeSpan.Days / 7 - years * 52 - months * 4;
+                returnString += weeks > 0 ? 
+                    FormatQuantity(weeks, TimeUnit.Week, textQuantity) + ", " : string.Empty;
+            }
+            else if (PrecisionMap[precision] == 2)
+            {
+                return FormatQuantity(timeSpan.TotalDays / 7, TimeUnit.Week, textQuantity);
+            }
+
+            if (PrecisionMap[precision] >= 3)
+            {
+                int days = timeSpan.Days - years * 365 - months * 30 - weeks * 7;
+                returnString += days > 0 ? 
+                    FormatQuantity(days, TimeUnit.Day, textQuantity) + ", " : string.Empty;
+                if (timeSpan.TotalDays < 1 && PrecisionMap[precision] == 3)
+                {
+                    return FormatQuantity(timeSpan.TotalDays, TimeUnit.Day, textQuantity);
+                }
+            }
+
+            if (PrecisionMap[precision] >= 4)
+            {
+                returnString += timeSpan.Hours > 0 ?
+                    FormatQuantity(timeSpan.Hours, TimeUnit.Hour, textQuantity) + ", " : string.Empty;
+                if (timeSpan.TotalHours < 1 && PrecisionMap[precision] == 4)
+                {
+                    return FormatQuantity(timeSpan.TotalHours, TimeUnit.Hour, textQuantity);
+                }
+            }
+
+            if (PrecisionMap[precision] >= 5)
+            {
+                returnString += timeSpan.Minutes > 0 ?
+                    FormatQuantity(timeSpan.Minutes, TimeUnit.Minute, textQuantity) + ", " : string.Empty;
+                if (timeSpan.TotalMinutes < 1 && PrecisionMap[precision] == 5)
+                {
+                    return FormatQuantity(timeSpan.TotalMinutes, TimeUnit.Minute, textQuantity);
+                }
+            }
+
+            if (PrecisionMap[precision] >= 6)
+            {
+                returnString += timeSpan.Seconds > 0 ?
+                    FormatQuantity(timeSpan.Seconds, TimeUnit.Second, textQuantity) : string.Empty;
+                if (timeSpan.TotalSeconds < 1 && PrecisionMap[precision] == 6)
+                {
+                    return FormatQuantity(timeSpan.TotalSeconds, TimeUnit.Second, textQuantity);
+                }
+            }
+
+            returnString = returnString.TrimEnd(',', ' ');
+            if (returnString.Contains(", "))
+            {
+                returnString = returnString.Remove(returnString.LastIndexOf(", "), 2)
+                    .Insert(returnString.LastIndexOf(", "), " and ");
+            }
+            return returnString;
+        }
+
+        /// <summary>
+        /// Formats a TimeSpan as text.
+        /// </summary>
+        /// <param name="timeSpan">The TimeSpan object to format.</param>
+        /// <param name="precision">The TimeUnit of precision to use.</param>
+        /// <returns>Returns a string representation of the TimeSpan.</returns>
+        public static string FormatTimespan(TimeSpan timeSpan, TimeUnit precision)
+        {
+            return FormatTimespan(timeSpan, precision, false);
+        }
+
+        /// <summary>
+        /// Formats a TimeSpan as text.
+        /// </summary>
+        /// <param name="timeSpan">The TimeSpan object to format.</param>
+        /// <returns>Returns a string representation of the TimeSpan.</returns>
+        public static string FormatTimespan(TimeSpan timeSpan)
+        {
+            return FormatTimespan(timeSpan, TimeUnit.Second, false);
+        }
+
+        #endregion
+
+        #region "Capitalize"
+
+        /// <summary>
+        /// Capitalizes one or all words in a string.
+        /// </summary>
+        /// <param name="text">The string to capitalize.</param>
+        /// <param name="allWords">Indicates whether all words detected should be capitalized.</param>
+        /// <returns>Returns a formatted string.</returns>
+        public static string Capitalize(string text, bool allWords)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException("String cannot be empty or null.");
+            }
+
+            if (allWords)
+            {
+                string textStr = string.Empty;
+                foreach (string word in text.Trim().Split(' '))
+                {
+                    textStr += CapitalizeString(word) + " ";
+                }
+                return textStr.Trim();
+            }
+            return CapitalizeString(text);
+        }
+
+        /// <summary>
+        /// Capitalizes one or all words in a string.
+        /// </summary>
+        /// <param name="text">The string to capitalize.</param>
+        /// <returns>Returns a formatted string.</returns>
+        public static string Capitalize(string text)
+        {
+            return Capitalize(text, true);
+        }
+
+        private static string CapitalizeString(string text)
+        {
+            return char.ToUpper(text[0]).ToString() + (text.Length > 1 ?
+                text.Substring(1) : string.Empty);
+        }
+
+        #endregion
     }
 }
---
+++ UX/TimeUnit.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crews.UX
+{
+    /// <summary>
+    /// A class used as a "string enumerable" type for passing units of time.
+    /// </summary>
+    public class TimeUnit
+    {
+        /// <summary>
+        /// Stored value of the class.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        ///  Built-in ToString method override.
+        /// </summary>
+        /// <returns>Returns a string representation of the class.</returns>
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// Type cast override for strings.
+        /// </summary>
+        /// <param name="unit">The TimeUnit object to use in the override.</param>
+        public static implicit operator string(TimeUnit unit)
+        {
+            return unit.Value;
+        }
+
+        private TimeUnit(string value) 
+        { 
+            Value = value; 
+        }
+
+        /// <summary>
+        /// TimeUnit object representing a year.
+        /// </summary>
+        public static TimeUnit Year { get { return new TimeUnit("year"); } }
+
+        /// <summary>
+        /// TimeUnit object representing a month.
+        /// </summary>
+        public static TimeUnit Month { get { return new TimeUnit("month"); } }
+
+        /// <summary>
+        /// TimeUnit object representing a week.
+        /// </summary>
+        public static TimeUnit Week { get { return new TimeUnit("week"); } }
+
+        /// <summary>
+        /// TimeUnit object representing a day.
+        /// </summary>
+        public static TimeUnit Day { get { return new TimeUnit("day"); } }
+
+        /// <summary>
+        /// TimeUnit object representing an hour.
+        /// </summary>
+        public static TimeUnit Hour { get { return new TimeUnit("hour"); } }
+
+        /// <summary>
+        /// TimeUnit object representing a minute.
+        /// </summary>
+        public static TimeUnit Minute { get { return new TimeUnit("minute"); } }
+
+        /// <summary>
+        /// TimeUnit object representing a second.
+        /// </summary>
+        public static TimeUnit Second { get { return new TimeUnit("second"); } }
+
+    }
+}
